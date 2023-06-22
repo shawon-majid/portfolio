2f3a5116e823af8e890ae8e6f711a33ae046e5a3
Shawon Majid <shawon.majid@gmail.com>
2023-06-22 21:35:36 +0600

    added google sheet api functionality

--- index.js
+++ index.js
@@ -16,3 +16,23 @@ function opentab(event, tabname) {
 
 }
 
+
+const scriptURL = 'https://script.google.com/macros/s/AKfycbx2pktKJgJrrzpJ8FkYXwSz4TJbdng_4ji5Wu6aICU84BGByPgBnLu-W9gegKEOz03u/exec';
+const form = document.forms['submit-to-google-sheet'];
+
+const msg = document.getElementById("msg");
+
+form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form) })
+        .then(response => {
+            msg.innerHTML = "Your message is sent successfully!"
+            setTimeout(function () {
+                msg.innerHTML = ""
+            }, 5000)
+            form.reset()
+        })
+        .catch(error => console.error('Error!', error.message))
+})
+
+
